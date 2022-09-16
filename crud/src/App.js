ddc8fb0130dc66d97434b45962ba959945a5576c
Giuliana Cabral <cabral.giu@hotmail.com>
2022-09-16 18:27:13 -0300

    Adicionada route Home

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div>
-      <h1>CRUD</h1>
-    </div>
+  return <>
+
+  <Routes>
+    <Route path="/" element={<Home />} />
+
+  </Routes>
+
+
+
+
+  </>
   );
 }
 
